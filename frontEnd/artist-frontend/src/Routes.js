9ededae7de2505e090a5238abfd4ec351e065f01
VinayKumarPrajapati <107835976+VinayKumarPrajapati@users.noreply.github.com>
2022-06-24 13:28:58 +0530

    added home route in frontend

--- frontEnd/artist-frontend/src/Routes.js
+++ frontEnd/artist-frontend/src/Routes.js
@@ -2,10 +2,12 @@ import react from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Register from './artist/register';
 import Login from './artist/login';
+import Home from './core/Home';
 
 const Routes = () => {
     return (<BrowserRouter>
         <Switch>
+            <Route path="/" exact component={Home} />
             <Route path="/register" exact component={Register} />
             <Route path="/login" exact component={Login} />
             
---
+++ frontEnd/artist-frontend/src/core/Home.js
@@ -0,0 +1,5 @@
+import react from 'react';
+
+const Home = () => <div>Home</div>;
+
+export default Home;
